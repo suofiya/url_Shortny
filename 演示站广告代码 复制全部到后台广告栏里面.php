1376db93bb253725105638de8303ca52bcea400a
图素设计 <1058364519@qq.com>
2020-07-28 19:34:50 +0800

    Add files via upload

---
+++ 演示站广告代码 复制全部到后台广告栏里面.php
@@ -0,0 +1,103 @@
+﻿<p><div align="center"></p><h4><!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>广告</title>
+</head>
+<body>
+<script src="js/slider.js"></script>
+<style>
+
+@keyframes jssorl-009-spin {
+	from {
+	transform:rotate(0);
+}
+to {
+	transform:rotate(360deg);
+}
+}.jssorb051 .i {
+	position:absolute;
+	cursor:pointer
+}
+.jssorb051 .i .b {
+	fill:#fff;
+	fill-opacity:.5
+}
+.jssorb051 .i:hover .b {
+	fill-opacity:.7
+}
+.jssorb051 .iav .b {
+	fill-opacity:1
+}
+.jssorb051 .i.idn {
+	opacity:.3
+}
+.jssora051 {
+	display:block;
+	position:absolute;
+	cursor:pointer
+}
+.jssora051 .a {
+	fill:none;
+	stroke:#fff;
+	stroke-width:360;
+	stroke-miterlimit:10
+}
+.jssora051:hover {
+	opacity:.8
+}
+.jssora051.jssora051dn {
+	opacity:.5
+}
+.jssora051.jssora051ds {
+	opacity:.3;
+	pointer-events:none
+}
+
+</style><!-- 广告图片尺寸默认为900X380,如果要修改图片尺寸请修改59行和63行的对应尺寸 -->
+<div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:900px;height:380px;overflow:hidden;visibility:hidden;"> 
+
+<!-- 广告图片及广告链接 -->
+<div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);"> <img style="margin-top:-0px;position:relative;top:50%;width:38px;height:0px;" src="img/spin.svg" /> </div>
+<div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:900px;height:380px;overflow:hidden;">	
+	<a target="_blank" href="https://www.aliyun.com/sale-season/2020/procurement-new-members?userCode=wkbvc5em"> <!-- 广告链接 -->
+    <img border="0" src="http://img.atusu.cn/images/2020/07/02/12dd.jpg" /><!-- 广告图片 -->
+    </a>    
+	<a target="_blank" href="https://www.aliyun.com/sale-season/2020/procurement-new-members?userCode=wkbvc5em"><!-- 广告链接 -->
+    <img border="0" src="http://img.atusu.cn/images/2020/07/02/12dd.jpg" /><!-- 广告图片 -->
+    </a>	
+	<a target="_blank" href="https://cloud.tencent.com/act/cps/redirect?redirect=1040&cps_key=b070a3592acb866f47857b85c2daa8de&from=console"><!-- 广告链接 -->
+    <img border="0" src="http://img.atusu.cn/images/2020/07/02/12dd.jpg" /><!-- 广告图片 -->
+    </a>
+	<a target="_blank" href="https://promotion.aliyun.com/ntms/act/enterprise-discount.html?userCode=wkbvc5em"><!-- 广告链接 -->
+    <img border="0" src="http://img.atusu.cn/images/2020/07/02/12dd.jpg" /><!-- 广告图片 -->
+    </a>
+	    
+</div>
+<!-- Bullet Navigator -->
+<div data-u="navigator" class="jssorb051" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+<div data-u="prototype" class="i" style="width:16px;height:16px;">
+<svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+<circle class="b" cx="8000" cy="8000" r="5800"></circle>
+</svg>
+</div>
+</div>
+<!-- Arrow Navigator -->
+<div data-u="arrowleft" class="jssora051" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+
+<polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
+</svg>
+</div>
+<div data-u="arrowright" class="jssora051" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+
+<polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
+</svg>
+</div>
+</div>
+<script type="text/javascript">jssor_1_slider_init();</script> 
+<!-- #endregion Jssor Slider End -->
+</body>
+</html>
+</h4> </p></div>
+
