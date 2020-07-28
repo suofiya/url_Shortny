8320c17df1def60e9239ccfb571882a5344d3e7d
图素设计 <1058364519@qq.com>
2020-07-28 20:16:48 +0800

    Update tos.php

--- tos.php
+++ tos.php
@@ -24,7 +24,7 @@ $info = $db->fetch_array($data);
         <link href="css/animate.css" rel="stylesheet">
 
         <link href="https://fonts.loli.net/css?family=Fjalla+One" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
         <style>
             <?php echo $info['cstm-style']; ?>
         </style>
