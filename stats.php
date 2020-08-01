e55baef140e94f05d20ec5bd3a32820000fb5352
图素设计 <1058364519@qq.com>
2020-08-01 11:56:42 +0800

    Update stats.php

--- stats.php
+++ stats.php
@@ -45,7 +45,7 @@ if ($url == '') {
             <link href="css/style.css" rel="stylesheet">
 
             <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+            <link rel="stylesheet" href="all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
             <link href="css/animate.css" rel="stylesheet">
 
             <style>
